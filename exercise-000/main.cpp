da1022799d393bc82efd4d2e72cfdd8016d9fbb7
AtasCem <atas.cem-it24@it.dhbw-ravensburg.de>
2025-09-30 14:45:19 +0200

    Add solutions for exercise-000

--- exercise-000/main.cpp
+++ exercise-000/main.cpp
@@ -3,6 +3,9 @@
 
 #include "config.h"
 
+void changeValueByParameter(int value);
+void changeValueByPointer(int* ptr);
+
 auto main(int argc, char **argv) -> int
 {
 
@@ -11,9 +14,49 @@ auto main(int argc, char **argv) -> int
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", tfe24::PROJECT_NAME);
+    fmt::print("Hello world, {}!\n", tfe24::PROJECT_NAME);
 
     /* INSERT YOUR CODE HERE */
+    fmt::print("\n\nPart 1 \n");
+    int x = 10;
+    int* p = &x;
+
+    fmt::print("Value of x: {}", x);
+    fmt::print("\nAddress of x: {}\n", fmt::ptr(p));
+
+    *p = 42;
+    fmt::print("New value of x: {}\n", x);
+    fmt::print("Address of new x: {}\n", fmt::ptr(p));
+
+    fmt::print("\n\nPart 2 \n");
+    double* d = new double(3.1415);
+
+    fmt::print("Value of d: {}\n", *d);
+    fmt::print("Address of d: {}\n", fmt::ptr(d));
+
+    delete d;
+
+    fmt::print("\n\nPart 3.1: Change by Parameter \n");
+    int value = 200;
+    fmt::print("Value before function call: {}\n", value);
+    changeValueByParameter(value);
+    fmt::print("Value after function call: {}\n", value);
+
+    fmt::print("\n\nPart 3.2: Change by Pointer \n");
+    int* ptr = &value;
+    fmt::print("Value before function call: {}\n", value);
+    changeValueByPointer(ptr);
+    fmt::print("Value after function call: {}\n", value);
 
     return 0; /* exit gracefully*/
+}
+
+ void changeValueByParameter(int value)
+ {
+    value = 99;
+}
+
+void changeValueByPointer(int* ptr)
+{
+    *ptr = 99;
 }

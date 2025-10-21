75f1ebf2120166980a38214d9629350ed1e4feb9
SimonStraub2005 <straub.simon-it24@it.dhbw-ravensburg.de>
2025-10-21 12:41:12 +0200

    zweite Übung

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -4,6 +4,13 @@
 #include "CLI/CLI.hpp"
 #include "config.h"
 
+int globalVar = 1;
+
+
+void foo() { 
+    fmt::print("Hello from foo!\\n"); 
+}
+
 auto main(int argc, char **argv) -> int
 {
     /**
@@ -20,7 +27,6 @@ auto main(int argc, char **argv) -> int
     {
         return app.exit(e);
     }
-
     /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
@@ -29,6 +35,24 @@ auto main(int argc, char **argv) -> int
     fmt::print("Hello, {}!\n", app.get_name());
 
     /* INSERT YOUR CODE HERE */
+    //Variabeln ab hier:
+    int localVar = 2;
+    int* heapVar = new int(3);
+
+    //code ab hier
+
+    fmt::print("Wert der globalen Variable: {}\n", globalVar);
+    fmt::print("Wert der lokalen Variable: {}\n", localVar);
+    fmt::print("Adresse der dynamischen Variable: {}\n", fmt::ptr(heapVar));
+    fmt::print("Wert der dynamischen Variable: {}\n", *heapVar);
+     
+    delete(heapVar);
+
+    fmt::print("Adresse von foo: {}\\n", fmt::ptr(&foo));
+
+
+
+
 
     return 0; /* exit gracefully*/
 }

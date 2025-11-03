983de6a16f8187229c95580bdc5f66f9386d0dca
Ferdinand Leybach <ferdinand@leybach.de>
2025-11-03 19:25:37 +0100

    added default programm strukture for getting command line args

--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -7,7 +7,24 @@
 #include <algorithm> /* needed for the std::sort */
 #include <random>
 
-int main() 
+
+int main(int argc, char** argv) 
 {
+    CLI::App app{"app, that can take a positive even integer as an inline input"};
+    argv = app.ensure_utf8(argv);
+
+    std::string filename = "default";
+    app.add_option("-f,--file", filename, "A help string");
+
+    try 
+    {
+    app.parse(argc, argv);
+    } 
+    catch (const CLI::ParseError &e) 
+    {
+    return app.exit(e);
+    }
     return 0;
 }
+
+

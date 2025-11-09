a5686586954a64a137d17ef1b02b805ed20e0a76
Ferdinand Leybach <ferdinand@leybach.de>
2025-11-09 20:53:46 +0100

    first commit

--- exercise-004/main.cpp
+++ exercise-004/main.cpp
@@ -7,31 +7,8 @@
 #include "point.hpp"
 #include "point.hpp"
 
-auto main(int argc, char **argv) -> int
+int main()
 {
-    /**
-     * CLI11 is a command line parser to add command line options
-     * More info at https://github.com/CLIUtils/CLI11#usage
-     */
-    CLI::App app{PROJECT_NAME};
-    try
-    {
-        app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
-        app.parse(argc, argv);
-    }
-    catch (const CLI::ParseError &e)
-    {
-        return app.exit(e);
-    }
 
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
-     * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
-     */
-    fmt::print("Hello, {}!\n", app.get_name());
-
-    /* INSERT YOUR CODE HERE */
-
-    return 0; /* exit gracefully*/
+    return 1;
 }

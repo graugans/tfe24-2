aaa8766a61bd74bd3cff7f6dcd14848ed9a38434
Ferdinand Leybach <ferdinand@leybach.de>
2025-11-10 21:18:46 +0100

    exercise-004/point.cpp, exercise-004/point.hpp: setter and getter fully deployed

--- exercise-004/point.cpp
+++ exercise-004/point.cpp
@@ -2,32 +2,6 @@
 #include <fmt/format.h>
 #include <cstdio>
 
-// Methoden (Getter)
-int Point::x()
-{
-    return px;
-}
-
-int Point::y()
-{
-    return py;
-}
-
-// Methoden (Setter)
-void Point::set(int x, int y)
-{
-    px = x;
-    py = y;
-    return;
-}
-
-void Point::move(int dx, int dy)
-{
-    px += dx;
-    py += dy;
-    return;
-};
-
 void Point::print() const
 {
     fmt::print("({},", px);
--- exercise-004/point.hpp
+++ exercise-004/point.hpp
@@ -1,25 +1,27 @@
 #ifndef POINT_HPP
 #define POINT_HPP
 
-
-
 class Point
 {
     private:
     int px;
-    int py; 
+    int py;
 
-// Getter / Setter -> set Methode sicherer als Initialisierung im Konstruktor
     public:
-    Point() : px(0), py(0) {};
-    int x();
-    int y();
-    Point(int x, int y) {};
-    void set(int xx, int yy);
-    void move(int dx, int dy);
+    Point() { set(0, 0); };
+    Point(int x, int y) { set(x, y); };
+
+    // Methoden: Getter
+    int x() const { return px; };
+    int y() const { return py; };
+
+    // Methoden: Setter
+    void set(int x, int y) { px = x; py = y; };
+
+    // Methoden: Manipulator
+    void move(int dx, int dy) { set(x()+dx, y()+dy); };
+
     void print() const;
 };
 
-
 #endif
-
